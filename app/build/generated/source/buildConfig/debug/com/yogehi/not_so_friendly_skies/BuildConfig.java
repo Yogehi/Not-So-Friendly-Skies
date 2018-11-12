273528bd1b764088f911392e56e0c2518cb199bd
Ken Gannon <yogehi@gmail.com>
2018-11-12 09:46:19 -0500

    updated gradle stuff yay

---
+++ app/build/generated/source/buildConfig/debug/com/yogehi/not_so_friendly_skies/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yogehi.not_so_friendly_skies;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yogehi.not_so_friendly_skies";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
